78f4103229c56f3ce283fe5921bb1b8c406689b8
t4gz <fulviansyah787@gmail.com>
2025-05-06 09:51:05 +0800

    edit koneksi

--- connector/koneksi_sk.php
+++ connector/koneksi_sk.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "sql105.infinityfree.com";
+$username = "if0_38890797";
+$password = "sehatikomputer";
 $database = "if0_38890797_sk";
 
 // Create connection
